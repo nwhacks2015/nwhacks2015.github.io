55e55805b2818bac0a1febf7da37d44d45764f0a
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-06-15 11:45:06 -0700

    Update registration.js

--- js/registration.js
+++ js/registration.js
@@ -1,66 +1,65 @@
-// // On page load...
-// $(function() {
+// On page load...
+$(function() {
 
-//   Parse.initialize("dHa1lqKqjUFf48Iw5tc4puKB0kbv1ONnejI6q91o","nEU6Qig9G0ldjBALkayRR5cMWQUc7qMXbjGjsGE0");
-
-//   // should really disable the return key 
-//   /*
-//   $('form').bind("keyup keypress", function(e) {
-//     var code = e.keyCode || e.which; 
-//     if (code  == 13) {               
-//       e.preventDefault();
-//       return false;
-//     }
-//   })*/
+  Parse.initialize("VQV5exDOHIgQBqAnOM9KtnlyG3IIJ6qIeFwHvPxA", "V9BggunFvfLpRPUMJJvQ1tK1EnPlffoubmnD6LIl");
+  // should really disable the return key 
+  /*
+  $('form').bind("keyup keypress", function(e) {
+    var code = e.keyCode || e.which; 
+    if (code  == 13) {               
+      e.preventDefault();
+      return false;
+    }
+  })*/
 
     
 
-//   $('#prereg-btn').click( function(){
-//     $("#prereg-btn").hide();
-//     $("#sponsor-btn").hide();
-//     $("form").show();
-//   });
-//   // handler for form submission
-//   $('#signup').on("click", function(event) {
-//     var $form = $('form');
+  $('#prereg-btn').click( function(){
+    $("#prereg-btn").hide();
+    $("#sponsor-btn").hide();
+    $("form").show();
+  });
+  // handler for form submission
+  $('#signup').on("click", function(event) {
+    var $form = $('form');
     
-//     var $target = $($form.attr('data-target'));
-//     console.log('hello');
-//     // Log the user out in case they're still logged in
-//     Parse.User.logOut();
+    var $target = $($form.attr('data-target'));
+    console.log('hello');
+    // Log the user out in case they're still logged in
+    Parse.User.logOut();
     
-//     var data = convertFormToJSON($form);
-//     data.password = Math.random().toString(36).substring(2);
-//     data.username = data.email;
+    var data = convertFormToJSON($form);
+    data.password = Math.random().toString(36).substring(2);
+    data.username = data.email;
     
-//     var user = new Parse.User();
-//     user.signUp(data, {
-//       success: function(user) {
-//         $("#prereg-btn").show();
-//         $("#sponsor-btn").show();
-//         $("#signup").hide();
-//         window.alert("Thanks for your showing your interest! You'll be the first to know when registration opens for nwHacks.");
+    var user = new Parse.User();
+    user.signUp(data, {
+      success: function(user) {
+        $("#prereg-btn").show();
+        $("#sponsor-btn").show();
+        $("#signup").hide();
+        window.alert("Thanks for your showing your interest! You'll be the first to know when registration opens for nwHacks.");
 
-//         window.setTimeout( $('form').hide(), 1000);
+        window.setTimeout( $('form').hide(), 1000);
 
-//       },
-//       error: function(user, error) {
-//         $("#signup").show();
-//         window.alert("There was an error with your application: " + error.message);
-//       }
-//     });
-//     event.preventDefault();
+      },
+      error: function(user, error) {
+        $("#signup").show();
+        window.alert("There was an error with your application: " + error.message);
+      }
+    });
+    event.preventDefault();
 
-//   });
-// });
+  });
+});
 
-// function convertFormToJSON(form){
-//   var array = $(form).serializeArray();
-//   var json = {};
+function convertFormToJSON(form){
+  var array = $(form).serializeArray();
+  var json = {};
   
-//   jQuery.each(array, function() {
-//       json[this.name] = this.value || '';
-//   });
+  jQuery.each(array, function() {
+      json[this.name] = this.value || '';
+  });
   
-//   return json;
-// }
+  return json;
+}
